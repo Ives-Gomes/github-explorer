e9f8c1400f883cc930d5bc1faaee51e8c5e5d738
Ives Moreira <ivesmoreira3@gmail.com>
2020-09-24 13:36:01 -0300

    Styles Repository's Page

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import { Header, RepositoryInfo, Issues } from './styles';
+
+import logoImg from '../../assets/logogithubexplorer.svg';
 
 interface RepositoryParams {
   repository: string;
@@ -8,7 +13,54 @@ interface RepositoryParams {
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
 
-  return <h1>{params.repository}</h1>;
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img src="https://avatars0.githubusercontent.com/u/53413719?s=460&u=1e98084c7754352365563418c0566299f52c7e39&v=4" alt="foto" />
+          <div>
+            <strong>Ives-Gomes/Ecoleta</strong>
+            <p>Descrição do repo</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Stars</span>
+          </li>
+
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+
+          <li>
+            <strong>67</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="#">
+          <div>
+            <strong>ddfsfsd</strong>
+            <p>dfsdfsdfsdf</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
